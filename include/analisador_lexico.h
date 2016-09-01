c209156824c5df59f989d4d2a790e0384035291d
Rocha Lima <jonasrla@gmail.com>
2016-09-01 02:41:26 -0300

    v0.7 analisador lexico

--- include/analisador_lexico.h
+++ include/analisador_lexico.h
@@ -1,1 +1,59 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+typedef enum {
+// palavras reservadas
+ARRAY, BOOLEAN, BREAK, CHAR, CONTINUE, DO, ELSE, FALSE, FUNCTION, IF, INTEGER, OF, STRING,
+STRUCT, TRUE, TYPE, VAR, WHILE,
+
+// simbolos
+COLON, SEMI_COLON, COMMA, EQUALS, LEFT_SQUARE, RIGHT_SQUARE, LEFT_BRACES, RIGHT_BRACES,
+LEFT_PARENTHESIS, RIGHT_PARENTHESIS, AND, OR, LESS_THAN, GREATER_THAN, LESS_OR_EQUAL,
+GREATER_OR_EQUAL, NOT_EQUAL, EQUAL_EQUAL, PLUS, PLUS_PLUS, MINUS, MINUS_MINUS, TIMES,
+DIVIDE, DOT, NOT,
+
+// tokens regulares
+CHARACTER, NUMERAL, STRINGVAL, ID,
+
+// token deconhecido
+UNKNOWN,
+
+// fim do arquivo
+END
+} t_token;
+
+typedef struct
+{
+    char type; //0-char, 1-int, 2-string
+    union{
+        char cVal;
+        int nVal;
+        char* sVal;
+    } _ ;
+} t_const;
+
+extern char nextChar;
+extern t_token token;
+extern int tokenSecundario;
+extern FILE* program;
+
+// Busca entre as palavras reservadas
+t_token searchKeyWord(string name);             
+
+// busca um token secundário
+int searchName(string name);                    
+
+int addCharConst(char c);                       //
+int addIntConst(int n);                         //
+int addStringConst(char* s);                   
+
+char getCharConst(int n);
+int  getIntConst(int n);
+string getStringConst(int n);
+
+char readChar();                                //
+
+t_token nextToken();                            //
--- src/analisadores/analisador_lexico.cpp
+++ src/analisadores/analisador_lexico.cpp
@@ -1,1 +1,236 @@
 #include "analisador_lexico.h"
+#include <list>
+
+using namespace std;
+
+char nextChar = '\x20';
+t_token token = UNKNOWN;
+int tokenSecundario = 0;
+FILE *program;
+list<t_const> constantList;
+
+
+t_token nextToken(){
+
+    while (isspace(nextChar)){
+        nextChar = readChar();
+    }
+    if (isalpha(nextChar)){
+        string text = "";
+        
+        do {
+            text += nextChar;
+            nextChar = readChar();
+        } while (isalnum(nextChar) || nextChar == '_');
+
+        token = searchKeyWord(text);
+        
+        if(token == ID)
+            tokenSecundario = searchName(text);
+
+    } else if (isdigit(nextChar)){
+        string numeral = "";
+        do {
+            numeral += nextChar;
+            nextChar = readChar();
+        } while (isdigit(nextChar));
+
+        token = NUMERAL;
+        tokenSecundario = addIntConst(stoi(numeral));
+
+    } else if (nextChar == '"') {
+        string str;
+        char * s;
+        do {
+            str += nextChar;
+            nextChar = readChar();
+        } while (nextChar != '"');
+
+        token = STRING;
+        s = str.c_str()
+        tokenSecundario = addStringConst(s);
+    } else if (nextChar == '\0') {
+        return END;
+    } else {
+        switch(nextChar) {
+            case '\'':
+                nextChar = readChar();
+                token = CHARACTER;
+                tokenSecundario = addCharConst(nextChar);
+                readChar();
+                nextChar = readChar();
+                break;
+     
+            case ':':
+                nextChar = readChar();
+                token = COLON;
+                break;
+
+            case ';':
+                nextChar = readChar();
+                token = SEMI_COLON;
+                break;
+
+            case ',':
+                nextChar = readChar();
+                token = COMMA;
+                break;
+
+            case '[':
+                nextChar = readChar();
+                token = LEFT_SQUARE;
+                break;
+
+            case ']':
+                nextChar = readChar();
+                token = RIGHT_SQUARE;
+                break;
+
+            case '{':
+                nextChar = readChar();
+                token = LEFT_BRACES;
+                break;
+
+            case '}':
+                nextChar = readChar();
+                token = RIGHT_BRACES;
+                break;
+
+            case '(':
+                nextChar = readChar();
+                token = LEFT_PARENTHESIS;
+                break;
+
+            case ')':
+                nextChar = readChar();
+                token = RIGHT_PARENTHESIS;
+                break;
+
+            case '&':
+                nextChar = readChar();
+                token = AND;
+                break;
+
+            case '|':
+                nextChar = readChar();
+                token = OR;
+                break;
+
+            case '*':
+                nextChar = readChar();
+                token = TIMES;
+                break;
+
+            case '/':
+                nextChar = readChar();
+                token = DIVIDE;
+                break;
+
+            case '.':
+                nextChar = readChar();
+                token = DOT;
+                break;
+
+            case '+':
+                nextChar = readChar();
+                if ( nextChar == '+'){
+                    token = PLUS_PLUS;
+                    nextChar = readChar();
+                } else {
+                    token = PLUS;
+                }
+                break;
+
+            case '!':
+                nextChar = readChar();
+                if ( nextChar == '='){
+                    token = NOT_EQUAL;
+                    nextChar = readChar();
+                } else {
+                    token = NOT;
+                }
+                break;
+
+            case '=':
+                nextChar = readChar();
+                if ( nextChar == '='){
+                    token = EQUAL_EQUAL;
+                    nextChar = readChar();
+                } else {
+                    token = EQUALS;
+                }
+                break;
+
+            case '-':
+                nextChar = readChar();
+                if ( nextChar == '-'){
+                    token = MINUS_MINUS;
+                    nextChar = readChar();
+                } else {
+                    token = MINUS;
+                }
+                break;
+
+            case '<':
+                nextChar = readChar();
+                if ( nextChar == '='){
+                    token = LESS_OR_EQUAL;
+                    nextChar = readChar();
+                } else {
+                    token = LESS_THAN;
+                }
+                break;
+
+            case '>':
+                nextChar = readChar();
+                if ( nextChar == '='){
+                    token = GREATER_OR_EQUAL;
+                    nextChar = readChar();
+                } else {
+                    token = GREATER_THAN;
+                }
+                break;
+            default:
+                token = UNKNOWN;
+        }
+    }
+    return token;
+}
+
+int searchName(string name){
+    return 0;
+}
+
+int addIntConst(int value){
+    t_const instance;
+    instance._.nVal = value;
+    constantList.push_back(instance);
+    return constantList.size();
+}
+
+int addCharConst(char value){
+    t_const instance;
+    instance._.cVal = value;
+    constantList.push_back(instance);
+    return constantList.size();
+}
+
+int addStringConst(char * s){
+    t_const instance;
+    instance._.sVal = s;
+    constantList.push_back(instance);
+    return constantList.size();
+}
+
+t_token searchKeyWord(string name){
+    return UNKNOWN;
+}
+
+char readChar(){
+    char res = fgetc(program);
+    if (res != EOF){
+        return res;
+    } else {
+        return '\0';
+    }
+}
