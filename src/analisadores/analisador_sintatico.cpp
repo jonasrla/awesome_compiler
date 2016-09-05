d95c6c8c81006238651cdc349915d93d4048f70a
Rocha Lima <jonasrla@gmail.com>
2016-09-05 12:18:38 -0300

    Fix identition

--- src/analisadores/analisador_sintatico.cpp
+++ src/analisadores/analisador_sintatico.cpp
@@ -155,20 +155,20 @@ void parse(){
         if(Action[q].count(a) < 1) {
             SyntaxError();
         } else {
-        p = Action[q][a];
-        if (IS_SHIFT(p)) {
-            PUSH(p);
-            a = nextToken();
-        } else if(IS_REDUCTION(p)) {
-            r = RULE(p);
-            POP(LEN[r]);
-            PUSH(Action[TOP()][LEFT[r]]);
-            // Semantics(r);
-        } else {
-            SyntaxError();
+            p = Action[q][a];
+            if (IS_SHIFT(p)) {
+                PUSH(p);
+                a = nextToken();
+            } else if(IS_REDUCTION(p)) {
+                r = RULE(p);
+                POP(LEN[r]);
+                PUSH(Action[TOP()][LEFT[r]]);
+                // Semantics(r);
+            } else {
+                SyntaxError();
+            }
+            q = TOP();
         }
-        q = TOP();
-    }
     } while(q != 300);
     printf("Passou\n");
 }
