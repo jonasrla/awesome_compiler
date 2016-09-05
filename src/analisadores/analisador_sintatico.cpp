9084483edcde8bdbce1bcdc39b057c6e67fd5e35
Rocha Lima <jonasrla@gmail.com>
2016-09-05 03:39:36 -0300

    copies parse implementation

--- src/analisadores/analisador_sintatico.cpp
+++ src/analisadores/analisador_sintatico.cpp
@@ -1,1 +1,28 @@
 #include "analisador_sintatico.h"
+void parse(){
+    PopulaAction();
+    int p = 0;
+    int q = 0;
+    int r = 0;
+    PUSH(0);
+    t_token a = nextToken();
+    do {
+        if(Action[q].count(a) < 1) {
+            SyntaxError();
+        } else {
+        p = Action[q][a];
+        if (IS_SHIFT(p)) {
+            PUSH(p);
+            a = nextToken();
+        } else if(IS_REDUCTION(p)) {
+            r = RULE(p);
+            POP(LEN[r]);
+            PUSH(Action[TOP()][LEFT[r]]);
+            // Semantics(r);
+        } else {
+            SyntaxError();
+        }
+        q = TOP();
+    }
+    } while(q != 1);
+}
