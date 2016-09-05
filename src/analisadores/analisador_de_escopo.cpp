5a88920317732864aa83e44c49081eb7dc27046f
Sombra <aluis.852@gmail.com>
2016-09-05 11:32:34 -0300

    analisador de escopo v1

--- src/analisadores/analisador_de_escopo.cpp
+++ src/analisadores/analisador_de_escopo.cpp
@@ -1,1 +1,140 @@
-#include "analisador_de_escopo.h"
+
+#include "escopo_tipos_codigo.hpp"
+#include <string>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+
+extern FILE *out;
+bool hasError = false;
+fpos_t functionVarPos;
+/*Semantic Stack*/
+stack<t_attrib> StackSem;
+/*Functions*/
+int nFuncs = 0;
+pobject curFunction;
+/*Symbol Tables*/
+pobject SymbolTable[MAX_NEST_LEVEL];
+pobject SymbolTableLast[MAX_NEST_LEVEL];
+int nCurrentLevel = 0;
+
+
+int newBlock(void){
+    SymbolTable[++nCurrentLevel] = NULL;
+    SymbolTableLast[nCurrentLevel] = NULL;
+    return nCurrentLevel;
+}
+
+int endBlock(void){
+    return --nCurrentLevel;
+}
+
+pobject define(int aName){
+    pobject obj = new object();
+    obj->nName = aName;
+    obj->pNext = NULL;
+    
+    if( SymbolTable[nCurrentLevel] == NULL){
+        SymbolTable[nCurrentLevel] = obj;
+        SymbolTableLast[nCurrentLevel] = obj;
+    }
+    else{
+        SymbolTableLast[nCurrentLevel]->pNext = obj;
+        SymbolTableLast[nCurrentLevel] = obj;
+    }
+    return obj;
+}
+
+pobject search (int aName){
+    pobject obj = SymbolTable[nCurrentLevel];
+    while(obj != NULL){
+        if(obj-> nName == aName)
+            break;
+        else
+            obj = obj->pNext;
+    }
+    return obj;
+}
+
+pobject find (int aName)
+{
+    int i;
+    pobject obj = NULL;
+    for( i = nCurrentLevel; i >= 0; --i ) {
+        obj = SymbolTable[i];
+        while( obj != NULL )
+        {
+            if( obj->nName == aName )
+                break;
+            else
+                obj = obj->pNext;
+        }
+        if( obj != NULL ) break;
+    }
+    return obj;
+}
+
+void Error(errorcode code){
+    hasError = true;
+    cout << "Linha: " << currentLine << " - ";
+    switch(code){
+        case ERR_NO_DECL:
+            cout << "Variavel nao declarada";
+            break;
+        case ERR_REDCL:
+            cout << "Variavel ja foi declarada";
+            break;
+        default:
+            break;
+    }
+    cout << endl;
+}
+
+void semantics(int rule){
+
+    switch(rule){
+        case IDD_RULE:
+            name = tokenSecundario;
+            IDD.name = name;
+            if( (p = search(name)) != NULL){
+                Error(ERR_REDCL);
+            }
+            else{
+                p = define(name);
+            }
+            
+            IDD.obj = p;
+            break;
+        case IDU_RULE:
+            name = tokenSecundario;
+            IDU.name = name;
+            
+            //cout << "TOKENSECUNDARIO" << name << endl;
+            if((p = find(name)) == NULL){
+                Error(ERR_NO_DECL);
+                p = define(name);
+            }
+            IDU.obj = p;
+            break;
+        case ID_RULE:
+            name = tokenSecundario;
+            ID.name = name;
+            ID.obj = NULL;
+            break;
+   
+        case NB_RULE:
+            newBlock();
+            break;
+        case DT_RULE:
+            endBlock();
+            break;
+
+        case DF_RULE:
+            endBlock();
+            break;
+        default:
+            break;
+    }
+}
