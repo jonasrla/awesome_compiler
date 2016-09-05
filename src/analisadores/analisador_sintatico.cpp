3559724699c941b066c570cc5e72283db9c438e3
Rocha Lima <jonasrla@gmail.com>
2016-09-05 03:42:56 -0300

    implements LEN and LEFT

--- src/analisadores/analisador_sintatico.cpp
+++ src/analisadores/analisador_sintatico.cpp
@@ -1,6 +1,8 @@
 #include "analisador_sintatico.h"
 #include <stack>
 stack<int> pilhaEstado;
+int LEN[] = {P, LDE, LDE, DE, DE, DT, DT, DT, TP, TP, TP, TP, TP, DC, DC, DF, LP, LP, B, LDV, LDV, LS, LS, DV, LI, LI, S, S, S, S, S, S, S, S, E, E, E, L, L, L, L, L, L, L, R, R, R, TM, TM, TM, F, F, F, F, F, F, F, F, F, F, F, F, F, F, LE, LE, LV, LV, LV, IDD, IDU, ID, NT_TRUE, NT_FALSE, NT_CHR, NT_STR, NT_NUM, NB};
+int LEFT[] = {1, 2, 1, 1, 1, 9, 8, 4, 1, 1, 1, 1, 1, 5, 3, 9, 5, 3, 4, 2, 1, 2, 1, 5, 3, 1, 5, 7, 5, 7, 1, 4, 2, 2, 3, 3, 1, 3, 3, 3, 3, 3, 3, 1, 3, 3, 1, 3, 3, 1, 1, 2, 2, 2, 2, 3, 4, 2, 2, 1, 1, 1, 1, 1, 3, 1, 3, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 void parse(){
     PopulaAction();
     int p = 0;
