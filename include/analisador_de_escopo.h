1c35d013b02e6b9ec2e3a103d011366fb2203927
Rocha Lima <jonasrla@gmail.com>
2016-10-03 02:54:44 -0300

    adds more types and atributes to object

--- include/analisador_de_escopo.h
+++ include/analisador_de_escopo.h
@@ -2,10 +2,43 @@
 
 #include "analisador_lexico.h"
 
-typedef struct object {
+typedef enum { 
+    NO_KIND_DEF_=-1, VAR_, PARAM_, FUNCTION_, FIELD_, ARRAY_TYPE_, STRUCT_TYPE_, ALIAS_TYPE_, SCALAR_TYPE_ , UNIVERSAL_
+} t_kind;
+
+typedef struct object
+{
     int nName;
     struct object *pNext;
-    struct obj;
+    t_kind eKind;
+    
+    union {
+        struct {
+            struct object *pType;
+            int nIndex;
+            int nSize;
+        } Var, Param, Field;
+        struct {
+            struct object *pRetType;
+            struct object *pParams;
+            int nIndex;
+            int nParams;
+            int nVars;
+        } Function;
+        struct {
+            struct object *pElemType; int nNumElems;
+            int nSize;
+        } Array;
+        struct {
+            struct object *pFields;
+            int nSize;
+        } Struct;
+        struct {
+            struct object *pBaseType;
+            int nSize;
+        } Alias,Type;
+    }_;
+    
 } object, *pobject;
 
 int NewBlock(void);
