c75403bf9ad83b7a1b7be60be3bfa7369b53b7ab
Rocha Lima <jonasrla@gmail.com>
2016-10-03 00:15:53 -0300

    stable analisador_de_escopo

--- include/analisador_de_escopo.h
+++ include/analisador_de_escopo.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "analisador_lexico.h"
+
+typedef struct object {
+    int nName;
+    struct object *pNext;
+    struct obj;
+} object, *pobject;
+
+int NewBlock(void);
+int EndBlock(void);
+pobject Define(int aName);
+pobject Search (int aName);
+pobject Find (int aName);
--- src/analisadores/analisador_de_escopo.cpp
+++ src/analisadores/analisador_de_escopo.cpp
@@ -1,35 +1,28 @@
-/*#include <string>
+#include <string>
 #include <iostream>
 #include <stack>
+#include "analisador_de_escopo.h"
 using namespace std;
 #define MAX_NEST_LEVEL 50
 
 extern FILE *out;
 bool hasError = false;
 
-
-typedef struct object
-{
-    int nName;
-    struct object *pNext;
-    struct obj;
-} object, *pobject;
-
 pobject    SymbolTable[MAX_NEST_LEVEL]; 
 pobject    SymbolTableLast[MAX_NEST_LEVEL]; 
 int        nCurrentLevel = 0; 
 
-int newBlock(void){
+int NewBlock(void){
     SymbolTable[++nCurrentLevel] = NULL;
     SymbolTableLast[nCurrentLevel] = NULL;
     return nCurrentLevel;
 }
 
-int endBlock(void){
+int EndBlock(void){
     return --nCurrentLevel;
 }
 
-pobject define(int aName){
+pobject Define(int aName){
     pobject obj = new object();
     obj->nName = aName;
     obj->pNext = NULL;
@@ -45,7 +38,7 @@ pobject define(int aName){
     return obj;
 }
 
-pobject search (int aName){
+pobject Search (int aName){
     pobject obj = SymbolTable[nCurrentLevel];
     while(obj != NULL){
         if(obj-> nName == aName)
@@ -56,8 +49,7 @@ pobject search (int aName){
     return obj;
 }
 
-pobject find (int aName)
-{
+pobject Find (int aName){
     int i;
     pobject obj = NULL;
     for( i = nCurrentLevel; i >= 0; --i ) {
@@ -73,72 +65,3 @@ pobject find (int aName)
     }
     return obj;
 }
-
-typedef enum {
-    ERR_REDCL,ERR_NO_DECL
-} errorcode;
-
-void Error(errorcode code){
-    hasError = true;
-    //cout << "Linha: " << currentLine << " - ";
-    switch(code){
-        case ERR_NO_DECL:
-            cout << "Variavel nao declarada";
-            break;
-        case ERR_REDCL:
-            cout << "Variavel ja foi declarada";
-            break;
-        default:
-            break;
-    }
-    cout << endl;
-}
-
-void semantics(int rule){
-    static object IDD,IDU,ID;
-    static int name
-    static pobject p;
-    switch(rule){
-        case IDD:
-            name = tokenSecundario;
-            IDD.nName = name;
-            if( (p = search(name)) != NULL){
-                Error(ERR_REDCL);
-            }
-            else{
-                p = define(name);
-            }
-            
-            IDD.obj = p;
-            break;
-        case IDU:
-            name = tokenSecundario;
-            IDU.nName = name;
-            
-            //cout << "TOKENSECUNDARIO" << name << endl;
-            if((p = find(name)) == NULL){
-                Error(ERR_NO_DECL);
-                p = define(name);
-            }
-            IDU.obj = p;
-            break;
-        case ID:
-            name = tokenSecundario;
-            ID.nName = name;
-            ID.obj = NULL;
-            break;
-   
-        case NB:
-            newBlock();
-            break;
-        case DT:
-            endBlock();
-            break;
-
-        case DF:
-            endBlock();
-            break;
-        default:
-            break;
-    }
-}*/
