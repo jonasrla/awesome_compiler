a930a9f3f7a406bcf156d9795e8273e282749211
Rocha Lima <jonasrla@gmail.com>
2016-10-05 02:27:01 -0300

    adds all rules

--- src/analisadores/analisador_de_atributos.cpp
+++ src/analisadores/analisador_de_atributos.cpp
@@ -137,41 +137,148 @@ void semantics(int rule){
     static pobject p,f,t;
     static t_attrib IDD_,IDU_,ID_,MC_,LE_,F_,DC_,MT_,T_,LI_,LI0_,LI1_,TRU_,FALS_,STR_,CHR_,NUM_,DC0_,DC1_,LP_,LP0_,LP1_,E_,E0_,E1_,L_,L0_,L1_,R_,R0_,R1_,K_,K0_,K1_,F0_,F1_,LV_,LV0_,LV1_,LE0_,LE1_,ME_,MW_;
     switch(rule){
-        case IDD_IDENTIFIER_RULE:
-            name = tokenSecundario;
-            IDD_.nont = IDD;
-            IDD_._.ID.name = name;
-            if( (p = Search(name)) != NULL){
-                Error(ERR_REDCL);
-            }
-            else{
-                p = Define(name);
-            }
-            p->eKind = NO_KIND_DEF_;
-            IDD_._.ID.obj = p;
-            StackSem.push_front(IDD_);
-            break;
-        case IDU_IDENTIFIER_RULE:
-            name = tokenSecundario;
-            IDU_.nont = IDU;
-            IDU_._.ID.name = name;
-            
-            if((p = Find(name)) == NULL){
-                Error(ERR_NO_DECL);
-                p = Define(name);
-            }
-            IDU_._.ID.obj = p;
-            StackSem.push_front(IDU_);
-            break;
-        case ID_IDENTIFIER_RULE:
-            ID_.nont = ID;
-            name = tokenSecundario;
-            ID_._.ID.name = name;
-            ID_._.ID.obj = NULL;
-            StackSem.push_front(ID_);
+        case TYPE_IDD_EQUALS_ARRAY_NT_NUM_OF_TP_RULE:  // DT -> TYPE IDD EQUALS ARRAY LEFT_SQUARE NT_NUM RIGHT_SQUARE OF TP
+
+            T_ = StackSem.front();
+            StackSem.pop_front();
+            IDD_ = StackSem.front();
+            StackSem.pop_front();
+            p = IDD_._.ID.obj;
+            t = T_._.T.type;
+            p->eKind = ALIAS_TYPE_;
+            p->_.Alias.pBaseType = t;
+
             break;
 
-        case IF_E_S_RULE:
+        case TYPE_IDD_EQUALS_STRUCT_NB_DC_RULE:        // DT -> TYPE IDD EQUALS STRUCT NB LEFT_BRACES DC RIGHT_BRACES
+
+            DC_ = StackSem.front();
+            StackSem.pop_front();
+            IDD_ = StackSem.front();
+            StackSem.pop_front();
+            p = IDD_._.ID.obj;
+            p->eKind = STRUCT_TYPE_;
+            p->_.Struct.pFields = DC_._.DC.list;
+            EndBlock();
+
+            break;
+
+        case TYPE_IDD_EQUALS_TP_RULE:                  // DT -> TYPE IDD EQUALS TP
+
+
+
+            break;
+
+        case INTEGER_RULE:                             // TP -> INTEGER
+
+
+
+            break;
+
+        case CHAR_RULE:                                // TP -> CHAR
+
+
+
+            break;
+
+        case BOOLEAN_RULE:                            // TP -> BOOLEAN
+
+
+
+            break;
+
+        case STRING_RULE:                             // TP -> STRING
+
+
+
+            break;
+
+        case TP_IDU_RULE:                             // TP -> IDU
+
+
+
+            break;
+
+        case DC_LI_TP_RULE:                           // DC -> DC SEMI_COLON LI COLON TP
+
+
+
+            break;
+
+        case LI_TP_RULE:                              // DC -> LI COLON TP
+
+
+
+            break;
+
+        case FUNCTION_IDD_NB_LP_TP_B_RULE:            // DF -> FUNCTION IDD NB LEFT_PARENTHESIS LP RIGHT_PARENTHESIS COLON TP MF B
+
+            EndBlock();
+
+            break;
+
+        case LP_IDD_TP_RULE:                          // LP -> LP COMMA IDD COLON TP
+
+
+
+            break;
+
+        case IDD_TP_RULE:                             // LP -> IDD COLON TP
+
+
+
+            break;
+
+        case LDV_LS_RULE:                             // B -> LEFT_BRACES LDV LS RIGHT_BRACES
+
+
+
+            break;
+
+        case LDV_DV_RULE:                             // LDV -> LDV DV
+
+
+
+            break;
+
+        case DV_RULE:                                 // LDV -> DV
+
+
+
+            break;
+
+        case LS_S_RULE:                               // LS -> LS S
+
+
+
+            break;
+
+        case S_RULE:                                  // LS -> S
+
+
+
+            break;
+
+        case VAR_LI_TP_RULE:                          // DV -> VAR LI COLON TP SEMI_COLON
+
+
+
+            break;
+
+        case LI_IDD_RULE:                             // LI -> LI COMMA IDD
+
+
+
+            break;
+
+        case IDD_RULE:                                // LI -> IDD
+
+
+
+            break;
+
+        case IF_E_S_RULE:                             // S -> IF LEFT_PARENTHESIS E RIGHT_PARENTHESIS S
+
             MT_ = StackSem.front();
             StackSem.pop_front();
             E_ = StackSem.front();
@@ -181,16 +288,185 @@ void semantics(int rule){
             if( !CheckTypes(t,pBool)){
                 Error(ERR_BOOL_TYPE_EXPECTED);
             }
-            
-            
-            break;
-        case NB_RULE:
-            NewBlock();
-            break;
-        case FUNCTION_IDD_NB_LP_TP_B_RULE:
-            EndBlock();
 
-        case IDU_LE_RULE:
+            break;
+
+        case IF_E_S_ELSE_S_RULE:                      // S -> IF LEFT_PARENTHESIS E RIGHT_PARENTHESIS S ELSE S
+
+
+
+            break;
+
+        case WHILE_E_S_RULE:                          // S -> WHILE LEFT_PARENTHESIS E RIGHT_PARENTHESIS S
+
+
+
+            break;
+
+        case DO_S_WHILE_E_RULE:                       // S -> DO S WHILE LEFT_PARENTHESIS E RIGHT_PARENTHESIS SEMI_COLON
+
+
+
+            break;
+
+        case B_RULE:                                  // S -> B
+
+
+
+            break;
+
+        case LV_EQUALS_E_RULE:                        // S -> LV EQUALS E SEMI_COLON
+
+
+
+            break;
+
+        case BREAK_RULE:                              // S -> BREAK SEMI_COLON
+
+
+
+            break;
+
+        case CONTINUE_RULE:                           // S -> CONTINUE SEMI_COLON
+
+
+
+            break;
+
+        case E_AND_L_RULE:                            // E -> E AND L
+
+
+
+            break;
+
+        case E_OR_L_RULE:                             // E -> E OR L
+
+
+
+            break;
+
+        case L_RULE:                                  // E -> L
+
+
+
+            break;
+
+        case L_LESS_THAN_R_RULE:                      // L -> L LESS_THAN R
+
+
+
+            break;
+
+        case L_GREATER_THAN_R_RULE:                   // L -> L GREATER_THAN R
+
+
+
+            break;
+
+        case L_LESS_OR_EQUAL_R_RULE:                  // L -> L LESS_OR_EQUAL R
+
+
+
+            break;
+
+        case L_GREATER_OR_EQUAL_R_RULE:               // L -> L GREATER_OR_EQUAL R
+
+
+
+            break;
+
+        case L_EQUAL_EQUAL_R_RULE:                    // L -> L EQUAL_EQUAL R
+
+
+
+            break;
+
+        case L_NOT_EQUAL_R_RULE:                      // L -> L NOT_EQUAL R
+
+
+
+            break;
+
+        case R_RULE:                                  // L -> R
+
+
+
+            break;
+
+        case R_PLUS_TM_RULE:                          // R -> R PLUS TM
+
+
+
+            break;
+
+        case R_MINUS_TM_RULE:                         // R -> R MINUS TM
+
+
+
+            break;
+
+        case TM_RULE:                                 // R -> TM
+
+
+
+            break;
+
+        case TM_TIMES_F_RULE:                         // TM -> TM TIMES F
+
+
+
+            break;
+
+        case TM_DIVIDE_F_RULE:                        // TM -> TM DIVIDE F
+
+
+
+            break;
+
+        case F_RULE:                                  // TM -> F
+
+
+
+            break;
+
+        case LV_RULE:                                 // F -> LV
+
+
+
+            break;
+
+        case PLUS_PLUS_LV_RULE:                       // F -> PLUS_PLUS LV
+
+
+
+            break;
+
+        case MINUS_MINUS_LV_RULE:                     // F -> MINUS_MINUS LV
+
+
+
+            break;
+
+        case LV_PLUS_PLUS_RULE:                       // F -> LV PLUS_PLUS
+
+
+
+            break;
+
+        case LV_MINUS_MINUS_RULE:                     // F -> LV MINUS_MINUS
+
+
+
+            break;
+
+        case F_E_RULE:                                // F -> LEFT_PARENTHESIS E RIGHT_PARENTHESIS
+
+
+
+            break;
+
+        case IDU_LE_RULE:                             // F -> IDU MC LEFT_PARENTHESIS LE RIGHT_PARENTHESIS
+
             std::cout << "IDU_LE_RULE " << currentLine << std::endl;
             LE_ = StackSem.front();
             StackSem.pop_front();
@@ -211,67 +487,167 @@ void semantics(int rule){
             F_.nont = F;
             StackSem.push_front(F_);
 
-        case TYPE_IDD_EQUALS_ARRAY_NT_NUM_OF_TP_RULE:
-            T_ = StackSem.front();
-            StackSem.pop_front();
-            IDD_ = StackSem.front();
-            StackSem.pop_front();
-            p = IDD_._.ID.obj;
-            t = T_._.T.type;
-            p->eKind = ALIAS_TYPE_;
-            p->_.Alias.pBaseType = t;
             break;
 
-        case TYPE_IDD_EQUALS_STRUCT_NB_DC_RULE:
-            DC_ = StackSem.front();
-            StackSem.pop_front();
-            IDD_ = StackSem.front();
-            StackSem.pop_front();
-            p = IDD_._.ID.obj;
-            p->eKind = STRUCT_TYPE_;
-            p->_.Struct.pFields = DC_._.DC.list;
-            EndBlock();
-          break;
-        
-        case DC_LI_TP_RULE:
-            T_ = StackSem.front();
-            StackSem.pop_front();
-            LI_ = StackSem.front();
-            StackSem.pop_front();
-            p = LI_._.LI.list;
-            t = T_._.T.type;
-            while( p != NULL && p->eKind == NO_KIND_DEF_ ) {
-                p->eKind = FIELD_;
-                p->_.Field.pType = t;
-                p = p->pNext;
-            }
-            DC_._.DC.list = LI_._.LI.list;
-            DC_.nont = DC;
-            StackSem.push_front(DC_);
+        case MINUS_F_RULE:                            // F -> MINUS F
+
+
+
             break;
 
-        case LI_TP_RULE:
-            T_ = StackSem.front();
-            StackSem.pop_front();
-            LI_ = StackSem.front();
-            StackSem.pop_front();
-            DC1_ = StackSem.front();
-            StackSem.pop_front();
-            p = LI_._.LI.list;
-            t = T_._.T.type;
-            while( p != NULL && p->eKind == NO_KIND_DEF_ ) {
-                p->eKind = FIELD_;
-                p->_.Field.pType = t;
-                p = p->pNext;
+        case NOT_F_RULE:                              // F -> NOT F
+
+
+
+            break;
+
+        case NT_TRUE_RULE:                            // F -> NT_TRUE
+
+
+
+            break;
+
+        case NT_FALSE_RULE:                           // F -> NT_FALSE
+
+
+
+            break;
+
+        case NT_CHR_RULE:                             // F -> NT_CHR
+
+
+
+            break;
+
+        case NT_STR_RULE:                             // F -> NT_STR
+
+
+
+            break;
+
+        case NT_NUM_RULE:                             // F -> NT_NUM
+
+
+
+            break;
+
+        case LE_COMMA_E_RULE:                         // LE -> LE COMMA E
+
+
+
+            break;
+
+        case LE_E_RULE:                               // LE -> E
+
+
+
+            break;
+
+        case LV_DOT_ID_RULE:                          // LV -> LV DOT ID
+
+
+
+            break;
+
+        case LV_E_RULE:                               // LV -> LV LEFT_SQUARE E RIGHT_SQUARE
+
+
+
+            break;
+
+        case LV_IDU_RULE:                             // LV -> IDU
+
+
+
+            break;
+
+        case IDD_IDENTIFIER_RULE:                     // IDD -> IDENTIFIER
+
+            name = tokenSecundario;
+            IDD_.nont = IDD;
+            IDD_._.ID.name = name;
+            if( (p = Search(name)) != NULL){
+                Error(ERR_REDCL);
             }
-            DC0_._.DC.list = DC1_._.DC.list;
-            p = DC0_._.DC.list;
-            while ( p!=NULL && p->pNext != NULL) {
-                p = p->pNext;
+            else{
+                p = Define(name);
             }
-            p->pNext = LI_._.LI.list;
-            DC0_.nont = DC;
-            StackSem.push_front(DC0_);
+            p->eKind = NO_KIND_DEF_;
+            IDD_._.ID.obj = p;
+            StackSem.push_front(IDD_);
+            break;
+
+        case IDU_IDENTIFIER_RULE:                     // IDU -> IDENTIFIER
+
+            name = tokenSecundario;
+            IDU_.nont = IDU;
+            IDU_._.ID.name = name;
+            
+            if((p = Find(name)) == NULL){
+                Error(ERR_NO_DECL);
+                p = Define(name);
+            }
+            IDU_._.ID.obj = p;
+            StackSem.push_front(IDU_);
+
+            break;
+
+        case ID_IDENTIFIER_RULE:                      // ID -> IDENTIFIER
+
+            ID_.nont = ID;
+            name = tokenSecundario;
+            ID_._.ID.name = name;
+            ID_._.ID.obj = NULL;
+            StackSem.push_front(ID_);
+
+            break;
+
+        case TRUE_RULE:                               // NT_TRUE -> TRUE
+
+
+
+            break;
+
+        case FALSE_RULE:                              // NT_FALSE -> FALSE
+
+
+
+            break;
+
+        case CHARACTER_RULE:                          // NT_CHR -> CHARACTER
+
+
+
+            break;
+
+        case STRINGVAL_RULE:                          // NT_STR -> STRINGVAL
+
+
+
+            break;
+
+        case NUMERAL_RULE:                            // NT_NUM -> NUMERAL
+
+
+
+            break;
+
+        case NB_RULE:                                   // NB -> ''
+
+            NewBlock();
+
+            break;
+
+        case MF_RULE:                                   // MF -> ''
+
+
+
+            break;
+
+        case MC_RULE:                                   // MC -> ''
+
+
+
             break;
         default:
             break;
